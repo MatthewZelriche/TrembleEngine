008b033a1010cf420e12b81034f9fae99933697d
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2024-12-22 17:52:49 -0600

    Initial commit

---
+++ build.py
@@ -0,0 +1,22 @@
+# Build script for building and packaging all artifacts and other resources
+import argparse
+import os
+import subprocess
+import shutil
+
+
+parser = argparse.ArgumentParser(prog='TrembleBuildScript')
+parser.add_argument('-c', '--config')
+
+args = parser.parse_args()
+config = args.config or "debug"
+
+subprocess.run(["dotnet", "build", "--configuration", "{}".format(config)])
+subprocess.run(["cargo", "build", "--manifest-path", "ext/tremble_lib/Cargo.toml"])
+
+# Copy library to output path
+print("Copying required files to build folder...")
+lib_ext = 'dll' if os.name == 'nt' else 'so'
+shutil.copyfile("ext/tremble_lib/target/{}/tremble_lib.{}".format(config, lib_ext), "out/bin/TrembleEngine/{}/tremble_lib.{}".format(config, lib_ext))
+
+print("Build completed successfully")

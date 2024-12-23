dd4dfcbfa27928def1d1254e9d7bad6a2b6cb4f2
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2024-12-23 11:28:29 -0600

    Get build script running on Linux

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Build script for building and packaging all artifacts and other resources
 import argparse
 import os
@@ -11,15 +12,18 @@ parser.add_argument('-c', '--config')
 args = parser.parse_args()
 config = args.config or "debug"
 
-subprocess.run(["dotnet", "build", "--configuration", "{}".format(config)])
+# Build rust lib first, as C# project relies on C# bindings to already exist
 if config == "release":
    subprocess.run(["cargo", "build", "--manifest-path", "ext/tremble_lib/Cargo.toml", "--release"])
 else:
    subprocess.run(["cargo", "build", "--manifest-path", "ext/tremble_lib/Cargo.toml"])
+subprocess.run(["dotnet", "build", "--configuration", "{}".format(config)])
 
 # Copy library to output path
 print("Copying required files to build folder...")
 lib_ext = 'dll' if os.name == 'nt' else 'so'
-shutil.copyfile("ext/tremble_lib/target/{}/tremble.{}".format(config, lib_ext), "out/bin/TrembleEngine/{}/tremble.{}".format(config, lib_ext))
+lib_prefix = '' if os.name == 'nt' else 'lib'
+shutil.copyfile("ext/tremble_lib/target/{}/{}tremble.{}".format(config, lib_prefix, lib_ext), 
+               "out/bin/TrembleEngine/{}/{}tremble.{}".format(config, lib_prefix, lib_ext))
 
 print("Build completed successfully")
